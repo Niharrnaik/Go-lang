e97c8fadd94005caad2b5ef2d89b56b8508c574f
Raptor_Halo <119489971+Niharrnaik@users.noreply.github.com>
2023-07-05 23:21:57 +0530

    Update Http prerequest.go

--- Http prerequest.go
+++ Http prerequest.go
@@ -1,84 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"sync"
-	"time"
+	"io/ioutil"
+	"net/http"
 )
 
-const (
-	numPhilosophers = 3
-	maxDiningCycles = 2
-)
+type Post struct {
+	UserID int `json:"userId"`
 
-type Philosopher struct {
-	id                  int
-	leftFork, rightFork *sync.Mutex
-	diningCycles        int
-}
-
-type DiningTable struct {
-	philosophers []*Philosopher
-	waiter       *sync.Mutex
-}
-
-func (p *Philosopher) think() {
-	fmt.Printf("Philosopher %d is thinking\n", p.id)
-	time.Sleep(time.Second)
-}
-
-func (p *Philosopher) eat() {
-	p.leftFork.Lock()
-	p.rightFork.Lock()
-
-	fmt.Printf("Philosopher %d is eating\n", p.id)
-	time.Sleep(time.Second)
-
-	p.rightFork.Unlock()
-	p.leftFork.Unlock()
-
-	p.diningCycles++
-}
-
-func (p *Philosopher) dine(table *DiningTable) {
-	for p.diningCycles < maxDiningCycles {
-		p.think()
-
-		table.waiter.Lock()
-		p.eat()
-		table.waiter.Unlock()
-	}
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
 }
 
 func main() {
-	table := &DiningTable{
-		philosophers: make([]*Philosopher, numPhilosophers),
-		waiter:       &sync.Mutex{},
+
+	url := "https://jsonplaceholder.typicode.com/posts/1"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
 	}
 
-	forks := make([]*sync.Mutex, numPhilosophers)
-	for i := 0; i < numPhilosophers; i++ {
-		forks[i] = &sync.Mutex{}
+	var post Post
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
 	}
 
-	for i := 0; i < numPhilosophers; i++ {
-		table.philosophers[i] = &Philosopher{
-			id:           i + 1,
-			leftFork:     forks[i],
-			rightFork:    forks[(i+1)%numPhilosophers],
-			diningCycles: 0,
-		}
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(numPhilosophers)
-
-	for _, philosopher := range table.philosophers {
-		go func(p *Philosopher) {
-			defer wg.Done()
-			p.dine(table)
-		}(philosopher)
-	}
-
-	wg.Wait()
+	fmt.Printf("Post ID: %d\n", post.ID)
+	fmt.Printf("User ID: %d\n", post.UserID)
+	fmt.Printf("Title: %s\n", post.Title)
+	fmt.Printf("Body: %s\n", post.Body)
 }
