008029ca6c573607699873fae414254a868e0748
Raptor_Halo <119489971+Niharrnaik@users.noreply.github.com>
2023-08-31 22:20:27 +0530

    Update Map.go

--- Map.go
+++ Map.go
@@ -1,36 +1,36 @@
 package main
-
 import "fmt"
-
 func main() {
-	myMap := make(map[string]int)
-
-	myMap["apple"] = 1
-	myMap["banana"] = 2
-	myMap["orange"] = 3
-
-	appleValue := myMap["apple"]
-	bananaValue := myMap["banana"]
-	fmt.Println("Value of apple : ", appleValue)
-	fmt.Println("Value of Banana : ", bananaValue)
-
-	myMap["apple"] = 5
-
-	fmt.Println("Updated value of apple : ", myMap["apple"])
-
-	delete(myMap, "orange")
-	fmt.Println("After deleting orange : ", myMap)
-
-	value, exists := myMap["banana"]
-	if exists {
-		fmt.Println("Value of banana : ", value)
-	} else {
-		fmt.Println("Banana not found in the map.")
-	}
-
-	for key, value := range myMap {
-		fmt.Println("Key : ", key, " Value : ", value)
-	}
-
-	fmt.Println("Length of the map : ", len(myMap))
+// Declare and initialize a map
+myMap := make(map[string]int)
+// Insert key-value pairs into the map
+myMap["apple"] = 1
+myMap["banana"] = 2
+myMap["orange"] = 3
+// Accessing values using keys
+appleValue := myMap["apple"]
+bananaValue := myMap["banana"]
+fmt.Println("Value of apple:", appleValue)
+fmt.Println("Value of banana:", bananaValue)
+// Updating values
+myMap["apple"] = 5
+fmt.Println("Updated value of apple:", myMap["apple"])
+// Deleting a key-value pair
+delete(myMap, "orange")
+fmt.Println("After deleting orange:", myMap)
+// Checking existence of a key
+value, exists := myMap["banana"]
+if exists {
+fmt.Println("Value of banana:", value)
+} else {
+fmt.Println("Banana not found in the map")
 }
+// Iterating over the map
+for key, value := range myMap {
+fmt.Println("Key:", key, "Value:", value)
+}
+// Length of the map
+fmt.Println("Length of the map:", len(myMap))
+}
+3
+rd M
