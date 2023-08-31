2a7ac69d67fdd18fb1603025285846e379b149e0
Raptor_Halo <119489971+Niharrnaik@users.noreply.github.com>
2023-08-31 22:21:14 +0530

    Update fibonaci.go

--- fibonaci.go
+++ fibonaci.go
@@ -1,20 +1,22 @@
 package main
+
 import "fmt"
 
-func main() {
-    var prev,current,sum int
-    prev=0
-    current =1
-    sum=0
-    for prev+current<40 {
-        next :=prev+current
-        //fmt.Println(next)
-        if next%2==0{
-            sum+=next
-        }
-        prev=current
-        current=next
+func fibonacci(n int) {
+    a, b := 0, 1
+
+    for i := 0; i < n; i++ {
+        fmt.Print(a, " ")
+
+        a, b = b, a+b
     }
-    
-  fmt.Println("The sum of the even valued terms is",sum)
+}
+
+func main() {
+    fmt.Print("Enter the number of terms: ")
+    var n int
+    fmt.Scan(&n)
+
+    fmt.Println("Fibonacci Series:")
+    fibonacci(n)
 }
